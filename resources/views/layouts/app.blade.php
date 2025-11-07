28ac1fb5847e08d96776c2f7afd0f4a4bdc8eabc
InvestUz <invest.abdusattorov@gmail.com>
2025-11-07 11:37:49 +0500

    upgrading

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -242,7 +242,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
-                            <span class="text-blue-700 font-semibold">{{ now()->format('d.m.Y H:i') }}</span>
+                            <span class="text-blue-700 font-semibold">{{ now()->format('d.m.Y') }}</span>
                         </div>
                     </div>
                 </div>
